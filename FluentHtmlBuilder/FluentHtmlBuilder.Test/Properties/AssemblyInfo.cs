bb32530598655168a95980e9a1660cfaf85b2183
Mark Antill <mark.antill@gmail.com>
2015-06-28 19:09:47 +0100

    Added some tests

---
+++ FluentHtmlBuilder/FluentHtmlBuilder.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluentHtmlBuilder.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FluentHtmlBuilder.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("468e2ae3-962e-47a7-84c1-737fff2559a0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FluentHtmlBuilder/FluentHtmlBuilder.Test/TagTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using NUnit;
+using NUnit.Framework;
+
+namespace FluentHtmlBuilder.Test
+{
+    [TestFixture]
+    public class TagTests
+    {
+        [Test]
+        public void EmptySpan()
+        {
+            var element = Tag.Element("span").ToString();
+            Assert.AreEqual("<span></span>", element);
+        }
+
+        [Test]
+        public void SpanWithContent()
+        {
+            var element = Tag.Element("span", "content").ToString();
+            Assert.AreEqual("<span>content</span>", element);
+        }
+
+        [Test]
+        public void InnerHtmlSingleAndEnumeration()
+        {
+            var items = new[] { "one", "two", "three" };
+            var list = Tag.Element("ul").WithInnerHtml(
+                          Tag.Element("li", "First Thing in list"),
+                          items.Select(i => Tag.Element("li", i))).ToString();
+
+            Assert.AreEqual("<ul><li>First Thing in list</li>\r\n<li>one</li>\r\n<li>two</li>\r\n<li>three</li></ul>", list);
+        }
+    }
+}
